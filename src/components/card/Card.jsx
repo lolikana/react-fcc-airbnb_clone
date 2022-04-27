dcdb2d46d37b19650efe25075aa167c89c50e635
Lolikana <thomas.guiart@gmail.com>
2022-04-27 15:27:32 +0900

    separate component card datas

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
-import Card1 from './../../images/Airbnb Cards/image 12.png';
-import Card2 from './../../images/Airbnb Cards/mountain-bike 1.png';
-import Card3 from './../../images/Airbnb Cards/wedding-photography 1.png';
+import cardDatas from './cardDatas';
 import { RiStarSFill } from 'react-icons/ri';
-
-const cardDatas = [
-  {
-    image: Card1,
-    statut: 'SOLD OUT',
-    stars: '5.0',
-    reviews: ' (6)',
-    place: 'USA',
-    info: 'Life lessins with Katie Zaferes',
-    price: '136',
-  },
-  {
-    image: Card3,
-    statut: 'ONLINE',
-    stars: '5.0',
-    reviews: ' (3)',
-    place: 'USA',
-    info: 'Learn wedding photography',
-    price: '125',
-  },
-  {
-    image: Card2,
-    statut: 'ONLINE',
-    stars: '4.8',
-    reviews: ' (2)',
-    place: 'USA',
-    info: 'Group Mountain Biking',
-    price: '50',
-  },
-];
-
 
 const Card = () => {
   return (
     <section className="section-card">
       {cardDatas.map(
-        ({ image, statut, stars, reviews, place, info, price }, index) => {
+        (
+          { image, statut, ratings, reviewsCount, country, title, price },
+          index
+        ) => {
           return (
             <div key={index} className="card-template">
               <div className="card-box">
@@ -59,12 +29,12 @@ const Card = () => {
               <div className="card-legend">
                 <div>
                   <RiStarSFill fill="red" className="star-icon" />
-                  {stars}{' '}
+                  {ratings}{' '}
                   <span>
-                    {reviews}・{place}
+                    ({reviewsCount})・{country}
                   </span>
                 </div>
-                <div className="card-info">{info}</div>
+                <div className="card-title">{title}</div>
                 <div className="card-price">
                   <strong>From ${price}</strong> / person
                 </div>
---
+++ src/components/card/cardDatas.jsx
@@ -0,0 +1,35 @@
+import Card1 from './../../images/Airbnb Cards/image 12.png';
+import Card2 from './../../images/Airbnb Cards/mountain-bike 1.png';
+import Card3 from './../../images/Airbnb Cards/wedding-photography 1.png';
+
+const cardDatas = [
+  {
+    image: Card1,
+    statut: 'SOLD OUT',
+    ratings: '5.0',
+    reviewsCount: '6',
+    country: 'USA',
+    title: 'Life lessons with Katie Zaferes',
+    price: '136',
+  },
+  {
+    image: Card3,
+    statut: 'ONLINE',
+    ratings: '5.0',
+    reviewsCount: '51',
+    country: 'USA',
+    title: 'Learn wedding photography',
+    price: '125',
+  },
+  {
+    image: Card2,
+    statut: 'ONLINE',
+    ratings: '4.8',
+    reviewsCount: '24',
+    country: 'USA',
+    title: 'Group Mountain Biking',
+    price: '50',
+  },
+];
+
+export default cardDatas;
